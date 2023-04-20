a18b880442c168bc1cec668cc37443873a9bb0a5
macbookpro <vuongquoc2002@gmail.com>
2023-04-20 13:15:04 +0700

    Tìm kiếm giao dịch trong trang History

--- app/src/main/java/com/example/moneymanagement/Views/HistoryFragment.java
+++ app/src/main/java/com/example/moneymanagement/Views/HistoryFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.model.AdapterClass;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.text.Editable;
