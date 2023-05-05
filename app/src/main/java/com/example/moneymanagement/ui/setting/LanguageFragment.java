71e6cefe58d568a154bebf999c0b166205b337c9
Tom <vanthuong00002@gmail.com>
2023-05-05 15:55:50 +0700

    change language

--- app/src/main/java/com/example/moneymanagement/ui/setting/LanguageFragment.java
+++ app/src/main/java/com/example/moneymanagement/ui/setting/LanguageFragment.java
@@ -1,6 +1,5 @@
 package com.example.moneymanagement.ui.setting;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,14 +13,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.moneymanagement.R;
-import com.example.moneymanagement.databinding.FragmentAccountsBinding;
 import com.example.moneymanagement.databinding.FragmentLanguageBinding;
-import com.example.moneymanagement.ui.setting.LanguageManagement;
 
 
 public class LanguageFragment extends Fragment {
     FragmentLanguageBinding binding;
-    LanguageManagement languageManagement = new LanguageManagement();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentLanguageBinding.inflate(inflater, container, false);
@@ -39,17 +35,13 @@ public class LanguageFragment extends Fragment {
         binding.engbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                languageManagement.updateResource(getActivity(),"en");
-                NavController navController = Navigation.findNavController(getActivity(), R.id.fragment);
-                navController.navigate(R.id.settingFragment);
+
             }
         });
         binding.viebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                languageManagement.updateResource(getActivity(), "vi");
-                NavController navController = Navigation.findNavController(getActivity(), R.id.fragment);
-                navController.navigate(R.id.settingFragment);
+
             }
         });
     }
