ba7169a178aabdbb227eb81c0f8bd2a9f402507d
VHDuc <ghostgreatful@gmail.com>
2023-05-06 11:34:02 +0700

    fix v3

--- app/src/main/java/com/example/moneymanagement/ui/accounts/AddAccountFragment.java
+++ app/src/main/java/com/example/moneymanagement/ui/accounts/AddAccountFragment.java
@@ -6,11 +6,15 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.moneymanagement.R;
 import com.example.moneymanagement.databinding.FragmentAddAccountBinding;
 
 import java.util.HashMap;
@@ -40,10 +44,21 @@ public class AddAccountFragment extends Fragment {
     private void initUI() {
         accountsViewModel = new AccountsViewModel();
         binding.confirmImg.setOnClickListener(view->{
-            Map<String, String> newAccount = new HashMap<>();
-            newAccount.put("Name", binding.accNameEdt.getText().toString().trim());
-            newAccount.put("Money", binding.accNumberEdt.getText().toString().trim());
-            accountsViewModel.addData(newAccount);
+            if(binding.accNameEdt.getText().toString().trim().length() == 0 || binding.accNumberEdt.getText().toString().trim().length() == 0){
+                Toast.makeText(requireActivity(), "You must input all fields!", Toast.LENGTH_LONG).show();
+            }
+            else{
+                Map<String, String> newAccount = new HashMap<>();
+                newAccount.put("Name", binding.accNameEdt.getText().toString().trim());
+                newAccount.put("Money", binding.accNumberEdt.getText().toString().trim());
+                accountsViewModel.addData(newAccount);
+            }
+
+        });
+
+        binding.cancelImg.setOnClickListener(view->{
+            NavController navController = Navigation.findNavController(requireActivity(), R.id.fragment);
+            navController.popBackStack();
         });
     }
 
